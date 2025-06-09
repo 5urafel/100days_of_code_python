92e8544a96bc74a18d66a60a57c4b41ef05d26cd
5urafel <surafel5540f@gmail.com>
2025-06-09 12:22:20 +0300

    Hangman completed

--- Day-07/hangman/main.py
+++ Day-07/hangman/main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 print('''
  _                                             
@@ -64,37 +65,53 @@ HANGMANPICS = ['''
 # random word ggenerator
 
 words = ["hangman", "computer", "python", "program", "gaming", "coding", "challenge"]
-
-index = random.randint(0, len(words) - 1)
-selected_word = words[index]
+selected_word = random.choice(words).lower()
 print(selected_word)
 
+def clean():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-blank = ''
-for letter in selected_word:
-        blank += "_ "
 
-updated_guessed_word = ""
-
-# for i in range(len(selected_word)):
-#     guessed_letter = input('Guess a letter: ')
-#     if selected_word[i] == guessed_letter:
-#         updated_guessed_word += guessed_letter  # If the letter matches, reveal it
-#     else:
-#         updated_guessed_word += blank[i]  # Otherwise, keep the current character
-# print( updated_guessed_word)
+reveal_word = "_ "*len(selected_word)
+def substitute(guessed_letter):
+    global reveal_word
+    for i in range (len(selected_word)):
+        if selected_word[i] == guessed_letter:
+            reveal_word = reveal_word[:i*2] + guessed_letter + reveal_word[i*2+1:]
+    return reveal_word
 
 
 
 
-# life_count = 0
-# while life_count < 7:
-#     guessed_letter = input('Guess a letter: ')
 
-    # else:
-    #     life_count += 1
-    #     print(f'you guessed {guessed_letter} ,that is not in the word, you lost a life')
-    #     print(HANGMANPICS[life_count ])
+life = len(HANGMANPICS)
+count = 0
+guessed_letter = input("gusse a letter:")
+while life > 0 :
+    substituted_word = substitute(guessed_letter)
+    print(substituted_word)
+    if guessed_letter in selected_word:
+        print(f"you guessed {guessed_letter}. it is in the word")
+        if count > 0:
+            print(HANGMANPICS[count])
+        if "_ " not in substituted_word:
+            print("You won the game")
+            break
+    else:
+        count += 1
+        if count >= 6:
+            print("Game over! You lost the game ")
+            break
+        print(f"you guessed letter {guessed_letter},that is not in the word .you lose a life.")
+        print(HANGMANPICS[count])
+        life -= 1
+
+    clean()
+    guessed_letter = input("gusse a letter:")
+
+
+
+
 
 
 
